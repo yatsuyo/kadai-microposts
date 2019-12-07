85277015fc3c32afe6c6ab7a47f03b24af0b417a
yoshiaki.yatsu <hungry_soul_0122@yahoo.co.jp>
2019-12-07 17:26:19 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -4,4 +4,10 @@
             <li class="ml-4">{{ $error }}</li>
         @endforeach
     </ul>
+    
+    <ul class="nav navbar-nav navbar-right">
+        <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+        <li><a href="#">Login</a></li>
+    </ul>
+                
 @endif
