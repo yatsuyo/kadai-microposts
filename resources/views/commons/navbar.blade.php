ef80aad7b44782c2cc4fc4fe361d9abef9fde454
yoshiaki.yatsu <hungry_soul_0122@yahoo.co.jp>
2019-12-07 17:41:38 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,13 +1,44 @@
-@if (count($errors) > 0)
-    <ul class="alert alert-danger" role="alert">
-        @foreach ($errors->all() as $error)
-            <li class="ml-4">{{ $error }}</li>
-        @endforeach
-    </ul>
-    
-    <ul class="nav navbar-nav navbar-right">
-        <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-        <li><a href="#">Login</a></li>
-    </ul>
-                
-@endif
+<?php /* @if (count($errors) > 0)
+//    <ul class="alert alert-danger" role="alert">
+//        @foreach ($errors->all() as $error)
+//            <li class="ml-4">{{ $error }}</li>
+//        @endforeach
+//    </ul>
+//    
+//    <ul class="nav navbar-nav navbar-right">
+//        <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+//        <li><a href="#">Login</a></li>
+//    </ul>
+//                
+//@endif
+*/ ?>
+
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark"> 
+        <a class="navbar-brand" href="/">Microposts</a>
+         
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                @if (Auth::check())
+                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li class="dropdown-item"><a href="#">My profile</a></li>
+                            <li class="dropdown-divider"></li>
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
+            </ul>
+        </div>
+    </nav>
+</header>
