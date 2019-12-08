2e8c4295644e9550b67713a21019f1764cdac652
yoshiaki.yatsu <hungry_soul_0122@yahoo.co.jp>
2019-12-08 14:28:49 +0000

    user pages

---
+++ app/Http/RedirectIfAuthenticated.php
@@ -0,0 +1,8 @@
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->check()) {
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
